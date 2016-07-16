539b737906a8553a2c7fab7728bf2b250c825bf4
soverway <sloverway@usfca.edu>
2016-07-15 21:28:13 -0700

    adding denoise

---
+++ denoise.py
@@ -0,0 +1,66 @@
+import sys
+from PIL import Image
+
+def denoise(img):
+	copyImage = img.copy()
+	originalMatrix = img.load() #makes a matrix from an image
+	pixels = copyImage.load()
+	width, height = img.size #width and height of image
+
+	for i in range(width):
+		for j in range(height):
+			r = region3x3(img, i, j) 
+			pixels[i, j] = median(r)
+
+	return copyImage
+
+def region3x3(img, x, y):
+	img.load()
+	list = [] 
+
+	# Broken into rows
+	list.append(getpixel(img, x-1, y-1))
+	list.append(getpixel(img, x, y-1))
+	list.append(getpixel(img, x+1, y-1))
+
+	list.append(getpixel(img, x-1, y))
+	list.append(getpixel(img, x, y))
+	list.append(getpixel(img, x+1, y))
+
+	list.append(getpixel(img, x-1, y+1))
+	list.append(getpixel(img, x, y+1))
+	list.append(getpixel(img, x+1, y+1)) 
+
+	return list
+
+def median(data):
+	sortedList = sorted(data)
+	midpoint = len(sortedList)/2
+
+	return sortedList[midpoint]
+
+
+def getpixel(img, x, y):
+	width, height = img.size
+	if (x < 0):
+		x = 0
+	if (x >= width):
+		x = width - 1
+	if (y >= height):
+		y = height - 1
+	if (y < 0):
+		y = 0
+	matrix = img.load()
+	return matrix[x, y]
+			
+
+
+if len(sys.argv)<=1:
+	print "missing image filename"
+	sys.exit(1)
+filename = sys.argv[1]
+img = Image.open(filename)
+img = img.convert("L")
+img.show()
+img = denoise(img)
+img.show() # call your denoise function
